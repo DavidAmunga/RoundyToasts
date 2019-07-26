7980697affaa9496c2db0c5e547782b1e380aabe
David Amunga <david.onyimbo@gmail.com>
2019-07-26 10:46:24 +0300

    Added Sample Code

--- app/src/main/java/com/amunga/roundytoasts/MainActivity.java
+++ app/src/main/java/com/amunga/roundytoasts/MainActivity.java
@@ -2,6 +2,7 @@ package com.amunga.roundytoasts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void failedToast(View view) {
+        new FailedToast(this, "This is Failure");
+    }
+
+    public void successToast(View view) {
+        new SuccessToast(this, "This is Success");
+
+    }
+
+    public void infoToast(View view) {
+        new InfoToast(this, "This is an Info");
+
     }
 }
